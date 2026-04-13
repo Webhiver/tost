014979c2f6e938af8a68dcb2ca478619ff76fff3
Ovidiu Dinu <dinu.ovidiu@gmail.com>
2026-04-13 12:26:35 +0300

    Add multi language support for English and Romanian (WIP)

--- app2/src/components/SatelliteMode.tsx
+++ app2/src/components/SatelliteMode.tsx
@@ -1,9 +1,11 @@
 import {GrSatellite} from "react-icons/gr";
 import {useContextSelector} from "@fluentui/react-context-selector";
 import {LocalContext} from "../_context";
+import {useIntl} from "react-intl";
 
 const SatelliteMode = () => {
 
+    const intl = useIntl();
     const healthy = useContextSelector(LocalContext, c => c.healthy)
     const targetTemp = useContextSelector(LocalContext, c => c.targetTemp)
     const temperature = useContextSelector(LocalContext, c => c.temperature)
@@ -14,31 +16,30 @@ const SatelliteMode = () => {
             <div className="flex-1 flex flex-col items-center justify-center gap-6">
                 <div className="bg-linear-to-r from-blue-100 dark:from-blue-800 to-transparent border border-blue-300 dark:border-blue-500 rounded-full text-blue-400 dark:text-blue-400 px-3 py-1 text-sm font-medium flex justify-start items-center gap-2">
                     <GrSatellite/>
-                    SATELLITE MODE
+                    {intl.formatMessage({id: "satelliteMode.title"})}
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                    <div className="text-lg font-extralight text-slate-500 dark:text-slate-400">Target Temperature</div>
+                    <div className="text-lg font-extralight text-slate-500 dark:text-slate-400">{intl.formatMessage({id: "satelliteMode.targetTemperature"})}</div>
                     <div className="text-4xl font-mono text-slate-400 dark:text-slate-300">{targetTemp?.toFixed(1) ?? "--"}°C
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                    <div className="text-lg font-extralight text-slate-500 dark:text-slate-400">Local Temperature</div>
+                    <div className="text-lg font-extralight text-slate-500 dark:text-slate-400">{intl.formatMessage({id: "satelliteMode.localTemperature"})}</div>
                     <div className="text-4xl font-mono text-slate-400 dark:text-slate-300">{temperature?.toFixed(1) ?? "--"}°C
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                    <div className="text-lg font-extralight text-slate-500 dark:text-slate-400">Local Humidity</div>
+                    <div className="text-lg font-extralight text-slate-500 dark:text-slate-400">{intl.formatMessage({id: "satelliteMode.localHumidity"})}</div>
                     <div className="text-4xl font-mono text-slate-400 dark:text-slate-300">{humidity?.toFixed(1) ?? "--"}%
                     </div>
                 </div>
                 <div className="flex flex-col items-center justify-center">
-                    {healthy && <div className="bg-linear-to-r from-emerald-100 dark:from-emerald-800 to-transparent border border-emerald-300 dark:border-emerald-500 rounded-full text-emerald-400 dark:text-emerald-400 px-3 py-1 text-sm font-medium">Sensor is healthy</div>}
-                    {!healthy && <div className="bg-linear-to-r from-amber-100 dark:from-amber-800 to-transparent border border-amber-300 dark:border-amber-500 rounded-full text-amber-400 dark:text-amber-400 px-3 py-1 text-sm font-medium">Sensor is broken</div>}
+                    {healthy && <div className="bg-linear-to-r from-emerald-100 dark:from-emerald-800 to-transparent border border-emerald-300 dark:border-emerald-500 rounded-full text-emerald-400 dark:text-emerald-400 px-3 py-1 text-sm font-medium">{intl.formatMessage({id: "satelliteMode.sensorHealthy"})}</div>}
+                    {!healthy && <div className="bg-linear-to-r from-amber-100 dark:from-amber-800 to-transparent border border-amber-300 dark:border-amber-500 rounded-full text-amber-400 dark:text-amber-400 px-3 py-1 text-sm font-medium">{intl.formatMessage({id: "satelliteMode.sensorBroken"})}</div>}
                 </div>
             </div>
             <div className="bg-slate-200 dark:bg-slate-700 p-4 rounded-lg text-slate-600 dark:text-slate-300">
-                This device is operating as a satellite sensor. Temperature and humidity values are being reported to the host thermostat.<br/><br/>
-                All configurations can be made on the host thermostat.
+                {intl.formatMessage({id: "satelliteMode.description"}, {br: <br/>})}
             </div>
         </div>
     );
